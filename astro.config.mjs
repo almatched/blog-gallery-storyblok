76e610cf44c88d6770da6a087c7f59519df7bc5f
Daniil <folco1999@gmail.com>
2024-02-05 15:32:17 +0100

    Added gallery page with Storyblok for management of images.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+        plugins: [basicSsl()],
+        server: {
+            https: true,
+        },
+    },
     integrations: [
         storyblok({
             accessToken: env.STORYBLOK_TOKEN,
@@ -13,6 +20,8 @@ export default defineConfig({
                 blogPost: 'storyblok/BlogPost',
                 blogPostList: 'storyblok/BlogPostList',
                 page: 'storyblok/Page',
+                image: 'storyblok/Image',
+                imageGrid: 'storyblok/ImageGrid',
             },
             apiOptions: {
                 region: 'eu',
